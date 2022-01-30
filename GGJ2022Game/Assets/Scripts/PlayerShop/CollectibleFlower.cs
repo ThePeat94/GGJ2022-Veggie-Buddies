2e7df77fd5250604f256b206c1e9df46dd3f69d7
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2022-01-30 14:06:27 +0100

    sfx

--- GGJ2022Game/Assets/Scripts/PlayerShop/CollectibleFlower.cs
+++ GGJ2022Game/Assets/Scripts/PlayerShop/CollectibleFlower.cs
@@ -6,12 +6,15 @@ namespace Nidavellir.PlayerShop
     public class CollectibleFlower : MonoBehaviour
     {
         [SerializeField] private int m_worth;
+        [SerializeField] private AudioClip m_collectSound;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<PlayerController>(out var playerController))
             {
                 PlayerInventory.Instance.CurrencyAmount += this.m_worth;
+                FindObjectOfType<OneShotSfxPlayer>()
+                    .PlayOneShot(this.m_collectSound);
                 Destroy(this.gameObject);
             }
         }
