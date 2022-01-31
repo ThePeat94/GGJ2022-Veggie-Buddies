af2def5ac8a60a31ba1e64877bb8e9da198d8d34
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2022-02-01 00:15:46 +0100

    flower sound fix

--- GGJ2022Game/Assets/Scripts/OneShotSfxPlayer.cs
+++ GGJ2022Game/Assets/Scripts/OneShotSfxPlayer.cs
@@ -8,17 +8,18 @@ namespace Nidavellir
     [RequireComponent(typeof(AudioListener))]
     public class OneShotSfxPlayer : MonoBehaviour
     {
-        public void PlayOneShot(AudioClip clipToPlay)
+        public void PlayOneShot(AudioClip clipToPlay, float volume = 1f)
         {
             if (clipToPlay == null)
                 return;
-            this.StartCoroutine(this.PlayClipAndDestroySource(clipToPlay));
+            this.StartCoroutine(this.PlayClipAndDestroySource(clipToPlay, volume));
         }
 
-        private IEnumerator PlayClipAndDestroySource(AudioClip clip)
+        private IEnumerator PlayClipAndDestroySource(AudioClip clip, float volume)
         {
             var audioSource = this.AddComponent<AudioSource>();
             audioSource.clip = clip;
+            audioSource.volume = volume;
             audioSource.Play();
             yield return new WaitForSeconds(clip.length);
             Destroy(audioSource);
--- GGJ2022Game/Assets/Scripts/PlayerShop/CollectibleFlower.cs
+++ GGJ2022Game/Assets/Scripts/PlayerShop/CollectibleFlower.cs
@@ -5,16 +5,16 @@ namespace Nidavellir.PlayerShop
 {
     public class CollectibleFlower : MonoBehaviour
     {
+        [Range(0.0f, 1.0f)] public float Volume;
+
         [SerializeField] private int m_worth;
         [SerializeField] private AudioClip m_collectSound;
-
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<PlayerController>(out var playerController))
             {
                 PlayerInventory.Instance.CurrencyAmount += this.m_worth;
-                FindObjectOfType<OneShotSfxPlayer>()
-                    .PlayOneShot(this.m_collectSound);
+                FindObjectOfType<OneShotSfxPlayer>().PlayOneShot(this.m_collectSound, this.Volume);
                 Destroy(this.gameObject);
             }
         }
